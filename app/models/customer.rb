9cf73763a7bf5c0e6a9a48cd8850ae2fc231115c
yamagisitutomu <ec2-user@ip-172-31-0-91.ap-northeast-1.compute.internal>
2023-11-25 04:22:10 +0000

    マイグレーション修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -15,7 +15,7 @@ class Customer < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   
-  validates :name, presence: true, length: {maximum: 30}
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
   validates :birthdate, presence: true
