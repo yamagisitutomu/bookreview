2bd418ca87d5e21efd8582384890db3496441f4b
yamagisitutomu <143819828+yamagisitutomu@users.noreply.github.com>
2024-01-08 15:20:11 +0000

    comment権限

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,5 +1,6 @@
 class Public::CommentsController < ApplicationController
   before_action :set_post, only: [:create, :destroy]
+  before_action :authorize_comment_owner, only: [:destroy]
   
   def create
     @comment = @post.comments.build(comment_params)
@@ -29,4 +30,11 @@ class Public::CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
   end
   
+  def authorize_comment_owner
+    @comment = @post.comments.find_by(id: params[:id])
+
+    unless @comment && @comment.customer == current_customer
+      redirect_to book_path(@post.book.isbn), alert: '権限がありません。'
+    end
+  end
 end
